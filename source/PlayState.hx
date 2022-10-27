458fb8883fd4d2dba5b6cb40c2e37ecedaec56f7
EnzoXD2 <96305131+EnzoXD2@users.noreply.github.com>
2022-10-27 11:54:44 -0300

    Android+Controls

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -588,6 +588,10 @@ class PlayState extends MusicBeatState
 		noteTypeMap = null;
 		eventPushedMap.clear();
 		eventPushedMap = null;
+		
+		#if android
+		addAndroidControls();
+		#end
 
 		// After all characters being loaded, it makes then invisible 0.01s later so that the player won't freeze when you change characters
 		// add(strumLine);
@@ -677,6 +681,11 @@ class PlayState extends MusicBeatState
 		timeBarBG.cameras = [camHUD];
 		timeTxt.cameras = [camHUD];
 		doof.cameras = [camHUD];
+		
+		#if android
+		addAndroidControls();
+		androidControls.visible = true;
+		#end
 
 		// if (SONG.song == 'South')
 		// FlxG.camera.alpha = 0.7;
